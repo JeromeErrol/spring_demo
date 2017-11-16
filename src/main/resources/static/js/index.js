60deea0e035c22ad7cdfb8d5991c8aac27700147
jerome <jerome.saltmarsh@gmail.com>
2017-11-16 22:05:24 +1100

    added modal

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,38 +1,109 @@
  $( document ).ready(onDocumentReady);
 
  function onDocumentReady(){
+    controller.init();
+ }
 
-    $.get( "/user", function( user ) {
-        $("#username_id").text(user.name);
-    });
-    $.get( "/bookmarks", function( response ) {
-           var bookmarks = response._embedded.bookmarks;
-           for (i = 0; i < bookmarks.length; i++) {
-                ui.addBookmark(bookmarks[i]);
-           }
-    });
-    $("#add_bookmark_button").click(function(){
-        var title = $("#add_bookmark_text").val();
-        var bookmarkString = JSON.stringify({title : title});
-          $.ajax({
-              url: '/bookmarks/custom', // your api url
-              method: 'POST', // method is any HTTP method
-              data: bookmarkString, // data as js object
-              contentType : "application/json",
-              error: function(message){
-                alert(message.responseText);
-              },
-              success: function(bookmark) {
-                ui.addBookmark(bookmark);
-              }
-          });
-    });
+ var model = {
+    user : {},
+    bookmarks : []
  }
 
  var ui = {
-    addBookmark: function(bookmark){
 
-        var bookmarkButton = $("<p>" + bookmark.title + "</p>");
-        $("#bookmarks").append(bookmarkButton);
+    init : function(){
+        $("#modal-delete-button").click(function(buttonClickedEvent){
+            var id = buttonClickedEvent.target.data("id");
+            controller.deleteBookmark(id)
+        });
+
+        $("#add_bookmark_button").click(function(){
+                var title = $("#add_bookmark_text").val();
+                var bookmarkString = JSON.stringify({title : title});
+                  $.ajax({
+                      url: '/bookmarks/custom', // your api url
+                      method: 'POST', // method is any HTTP method
+                      data: bookmarkString, // data as js object
+                      contentType : "application/json",
+                      error: function(message){
+                        alert(message.responseText);
+                      },
+                      success: function(bookmark) {
+                        ui.addBookmark(bookmark);
+                      }
+                  });
+            });
+    },
+
+    refreshBookmarks : function(){
+        $("#bookmarks").empty();
+
+        for (i = 0; i < model.bookmarks.length; i++) {
+            var bookmark = model.bookmarks[i];
+            var bookmarkButton = $("<button " + attr("type", "button") + attr("class","btn btn-info btn-lg") + attr("data-toggle", "modal") +  attr("data-target", "#myModal") + ">" + bookmark.title + "</button>");
+            bookmarkButton.data("id", bookmark.id);
+            bookmarkButton.click(function(buttonClickedEvent){
+                $("#modal-body-text").val(buttonClickedEvent.target.textContent);
+            });
+            $("#bookmarks").append(bookmarkButton);
+        }
+    },
+
+    refreshUser : function(){
+        $("#username_id").text(model.user.name);
     }
+ }
+
+ var controller = {
+    init : function(){
+        $.ajaxSetup({
+            beforeSend : function(xhr, settings) {
+                if (settings.type == 'POST' || settings.type == 'PUT'
+                  || settings.type == 'DELETE') {
+                    if (!(/^http:.*/.test(settings.url) || /^https:.*/
+                    .test(settings.url))) {
+                    // Only send the token to relative URLs i.e. locally.
+                    xhr.setRequestHeader("X-XSRF-TOKEN", Cookies.get('XSRF-TOKEN'));
+                }
+              }
+            }
+        });
+        
+        controller.getUser();
+        controller.getBookmarks();
+    },
+
+    deleteBookmark : function(id){
+        alert("deleting " + id);
+
+        $.ajax({
+                      url: '/bookmarks/' + id, // your api url
+                      method: 'DELETE', // method is any HTTP method
+                      contentType : "application/json",
+                      error: function(message){
+                        alert(message.responseText);
+                      },
+                      success: function(bookmark) {
+                        alert(bookmark);
+                      }
+                  });
+    },
+
+    getBookmarks : function(){
+        $.get( "/bookmarks", function( response ) {
+            model.bookmarks = response._embedded.bookmarks;
+            ui.refreshBookmarks();
+        });
+    },
+
+    getUser : function(user){
+        $.get( "/user", function( user ) {
+            model.user = user;
+            ui.refreshUser();
+        });
+    }
+ }
+
+ function attr(name, value){
+    return name + "='" + value + "'";
  }
